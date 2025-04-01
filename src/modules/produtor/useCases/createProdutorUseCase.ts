cf24185600d2e35187623ce98fe989ee129987ff
Leonardo Richard <leonardo.richard@pm.pe.gov.br>
2025-04-01 20:53:36 -0300

    chore: finished class

---
+++ src/modules/produtor/useCases/createProdutorUseCase.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { Cultura, Produtor } from '../entities/Produtor';
+import { ProdutorRepository } from '../repositories/ProdutorRepository';
+
+interface CreateProdutorRequest {
+  cpfOuCnpj: string;
+  nomeProdutor: string;
+  nomeFazenda: string;
+  cidade: string;
+  estado: string;
+  areaTotal: number;
+  areaAgricultavel: number;
+  areaVegetacao: number;
+  culturas: Cultura[];
+  createdAt: Date;
+}
+
+let produtorRepository: ProdutorRepository;
+let produtor: Produtor;
+
+@Injectable()
+export class createProdutorUseCase {
+  constructor(private produtorRepository: ProdutorRepository) { }
+  async execute({
+    cpfOuCnpj,
+    nomeProdutor,
+    nomeFazenda,
+    cidade,
+    estado,
+    areaTotal,
+    areaAgricultavel,
+    areaVegetacao,
+    culturas,
+  }: CreateProdutorRequest) {
+    const produtor = new Produtor({
+      cpfOuCnpj,
+      nomeProdutor,
+      nomeFazenda,
+      cidade,
+      estado,
+      areaTotal,
+      areaAgricultavel,
+      areaVegetacao,
+      culturas,
+    });
+
+    await this.produtorRepository.create(produtor);
+  }
+}
