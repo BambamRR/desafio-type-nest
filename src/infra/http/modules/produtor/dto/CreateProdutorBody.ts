7767929e8211048e486bcba9df33d6d96e0e5667
Leonardo Richard <leonardo.richard@pm.pe.gov.br>
2025-04-03 14:52:48 -0300

    chore: added types to CreateProdutorBody

--- src/infra/http/modules/produtor/dto/CreateProdutorBody.ts
+++ src/infra/http/modules/produtor/dto/CreateProdutorBody.ts
@@ -1,13 +1,32 @@
-import { Cultura } from 'src/modules/produtor/entities/Produtor';
+import { Cultura } from '@prisma/client';
+import { IsArray, IsEnum, IsNumber, IsString } from 'class-validator';
 
 export class CreateProdutorBody {
+  @IsString()
   cpfOuCnpj: string;
+
+  @IsString()
   nomeProdutor: string;
+
+  @IsString()
   nomeFazenda: string;
+
+  @IsString()
   cidade: string;
+
+  @IsString()
   estado: string;
+
+  @IsNumber()
   areaTotal: number;
+
+  @IsNumber()
   areaAgricultavel: number;
+
+  @IsNumber()
   areaVegetacao: number;
+
+  @IsArray()
+  @IsEnum(Cultura, { each: true })
   culturas: Cultura[];
 }
