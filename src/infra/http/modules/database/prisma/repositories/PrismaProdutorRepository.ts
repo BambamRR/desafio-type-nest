8accc607ef5f9172bfc5f2a6894ea292c492daa0
Leonardo Richard <leonardo.richard@pm.pe.gov.br>
2025-04-03 23:31:31 -0300

    chore: create all methods functitons

--- src/infra/http/modules/database/prisma/repositories/PrismaProdutorRepository.ts
+++ src/infra/http/modules/database/prisma/repositories/PrismaProdutorRepository.ts
@@ -6,7 +6,24 @@ import { PrismaProdutorMapper } from '../mappers/PrismaProdutorMapper';
 
 @Injectable()
 export class PrismaProdutorRepository implements ProdutorRepository {
-  constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
+
+  async findAll(): Promise<Produtor[]> {
+    const produtores = await this.prisma.produtor.findMany({});
+    return produtores.map((prismaProdutor) =>
+      PrismaProdutorMapper.toDomain(prismaProdutor),
+    );
+  }
+
+  async findById(id: string): Promise<Produtor> {
+    throw new Error('Method not implemented.');
+  }
+  async update(produtor: Produtor): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  async delete(id: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
   async create(produtor: Produtor): Promise<void> {
     const produtorRaw = PrismaProdutorMapper.toPrisma(produtor);
     await this.prisma.produtor.create({
