f1a4193a6eeb3441f8113f8310c5fbb1cfcac7d5
Leonardo Richard <leonardo.richard@pm.pe.gov.br>
2025-04-03 23:31:59 -0300

    chore: created Get to all Produtor entity

--- src/infra/http/modules/produtor/produtor.controller.ts
+++ src/infra/http/modules/produtor/produtor.controller.ts
@@ -1,11 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateProdutorBody } from './dto/CreateProdutorBody';
 import { CreateProdutorUseCase } from 'src/modules/produtor/useCases/createProdutorUseCase/createProdutorUseCase';
 import { ProdutorViewModel } from './viewModel/produtorViewModel';
+import { Produtor } from 'src/modules/produtor/entities/Produtor';
+import { FindAllProdutorUseCase } from 'src/modules/produtor/useCases/findAllProdutorUseCase/findAllProdutorUseCase.';
 
 @Controller('produtor')
 export class ProdutorController {
-  constructor(private createProdutorUseCase: CreateProdutorUseCase) {}
+  constructor(
+    private createProdutorUseCase: CreateProdutorUseCase,
+    private findAllProdutorUseCase: FindAllProdutorUseCase,
+  ) { }
 
   @Post()
   async createPost(@Body() body: CreateProdutorBody) {
@@ -33,5 +38,11 @@ export class ProdutorController {
     });
 
     return ProdutorViewModel.toView(produtor);
+  }
+
+  @Get()
+  async findAll() {
+    const produtores = await this.findAllProdutorUseCase.execute();
+    return produtores.map(ProdutorViewModel.toView);
   }
 }
